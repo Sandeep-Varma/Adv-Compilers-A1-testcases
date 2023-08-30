141b23b97a08dc977817d199441276505d00ce5a
Sandeep-Varma <dsv6799@gmail.com>
2023-08-31 03:46:14 +0530

    Added array assignment and array lookup cases

---
+++ t6.java
@@ -0,0 +1,28 @@
+class t5 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
+
+class A{
+    int x;
+    public int func(){
+        int[] a;
+        Boolean b;
+        int c;
+        c[b]=b; // 3 errors
+        c[c]=c; // 2 errors
+        c[c]=b; // 2 errors
+        c[b]=c; // 3 errors
+        a[b]=b; // 2 errors
+        a[b]=c; // 2 errors
+        a[c]=c; // 0 errors
+        a[c]=b; // 1 errors
+        c=a[b]; // 2 errors
+        c=a[c]; // 0 errors
+        a=c[b]; // 3 errors
+        a=a[c]; // 1 errors
+        // Total errors = 21
+        return c;
+    }
+}
